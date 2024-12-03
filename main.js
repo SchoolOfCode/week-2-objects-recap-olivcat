4c544e6019a2be90e21d885e71b49c1295840a65
“olivcat” <184215778+olivcat@users.noreply.github.com>
2024-12-03 16:48:45 +0000

    Task 1 - Declaring Objects

---
+++ main.js
@@ -0,0 +1,34 @@
+// My cat is black, his name is Tony, and he loves cuddles.
+const myCat = {
+  color: "black",
+  name: "Tony",
+  lovesCuddles: true,
+};
+
+// My house has 2 stories, no parking, 2 bedrooms, a garden and an energy efficiency rating of D.
+
+const myHouse = {
+  floors: 2,
+  parking: true,
+  bedrooms: 2,
+  garden: true,
+  energyEffciencyRating: "D",
+};
+
+// My car is red, has 4 wheels, power steering, and 2 seats, and does 20 miles per gallon.
+
+const myCar = {
+  color: "red",
+  numberOfWheels: 4,
+  powerSteering: true,
+  numberOfSeats: 2,
+  MPG: 20,
+};
+
+// My favourite film's title is The Dark Knight, it was released in 2008, and the lead actor is Christian Bale.
+
+const myFavouriteFilm = {
+  title: "The Dark Knight",
+  releaseDate: "2008",
+  leadActor: "Christian Bale",
+};
